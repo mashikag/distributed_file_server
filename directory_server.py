cb4dafc484aff091dca9e3c0be30eb6a86cb0c8c
Maciej Grabowski <littlesoldiergraba@gmail.com>
2016-03-13 19:40:13 +0000

    Directory Server fully functional now. Starting implementign functionality of the file server and adding on functionality to the client.

--- directory_server.py
+++ directory_server.py
@@ -3,26 +3,10 @@ import socket
 import sys
 import random
 import hashlib
+import re
 from threading import Thread
 from Queue import Queue
 
-"""
-The dir server will need to listen for requests to get and write to files(paths)
-it will need to store address + port num of file servers and id of files associate with them and paths associate with the ids
-hold dictionary of key:path, value:id;ip:port  
-
-receive write request
-  if path exists in the dict
-    return the id and the address of the file server associated
-  if doesnt
-    create a new entry in the dic for the path
-    return the address of the file server associated in the new entry
-receive get request
-  if path exists in the dict
-    return the id and the address of the file server associated
-  else
-    return not found error message
-"""
 
 STUDENT_ID = '<To be filled in>'
 IP = 'some IP'
@@ -43,8 +27,6 @@ class Worker(Thread):
             self.cmdParser(conn)
             self.tasks.task_done()
 
-
-
 class ThreadPool:
     """pool of worker threads all consuming tasks"""
 
@@ -57,12 +39,14 @@ class ThreadPool:
     def addTasks(self, conn):
         #put a new connection on the queue
         self.tasks.put(conn)
+        
+        
 
 class DirectoryServer:
     HOST = ''
-    PATH_REG_EX = r'\s*[a-zA-Z\\/0-9]+\s*'
+    PATH_REG_EX = r'-\s*path\s*[a-zA-Z\\/0-9]+\s*'
     KILL_CMD = r'\s*KILL\s+SERVICE\s*'
-    NEW_FILE_SERVER_BEACON = r'\s*file_server:\s*[.0-9]*:[0-9]+\s*'
+    NEW_FILE_SERVER_BEACON = r'\s*file_server=\s*[.0-9]*:[0-9]+\s*'
     SERVER_NAME = "directory_server"
 
     def __init__(self, numThread, port):
@@ -78,6 +62,7 @@ class DirectoryServer:
         print 'Socket successfully bound.'
         self.threadPool = ThreadPool(numThread, self)
         
+        
         self.pathReqRegEx = re.compile(self.PATH_REG_EX)
         self.newFileServerBeacon = re.compile(self.NEW_FILE_SERVER_BEACON)
         self.killCmdRegEx = re.compile(self.KILL_CMD)
@@ -92,45 +77,49 @@ class DirectoryServer:
         print 'Server listening on port: ' + str(self.port)
         while True:
             connection, address = self.sock.accept()
-            print 'Incoming connection established with ' + addr[0] + ':' + str(addr[1])
+            print 'Incoming connection established with ' + address[0] + ':' + str(address[1])
             self.threadPool.addTasks(connection)
 
     def cmdParser(self, conn):
-        while conn:
-            data = conn.recv(2048)
-            if self.killCmdRegEx.match(data):
-                print 'Shutting down...'
-                conn.sendall("Shutting down the Directory Server...")
-                sys.exit()
-            elif self.pathReqRegEx.match(data):
-                print 'file server data requested for a path'
-                path = data.strip()
-                fileServerAddress = ''
-                fileServerFileId = ''
-                fileServerAddress, fileServerFileId = getFileServerData(path)
-                if fileServerAddress is not None and fileServerFileId is not None:
-                    response = generateResponse(fileServerFileId + ";" + fileServerAddress)
-                else:
-                    response = generateResponse("101-ERROR: unnable to get file server data. Possibly directory server is unaware of any running file servers. ")
-                conn.sendall(response)
-            elif self.newFileServerBeacon.match(data):
-                print 'received new file server beacon'
-                """TO DO NEXT"""
-                """TO DO NEXT"""
-                """TO DO NEXT"""
-                """TO DO NEXT"""
-                """TO DO NEXT"""
-                """TO DO NEXT"""
-            else:
-                print 'unsupported command received'
-                response = generateResponse("100-ERROR: unsupported command received")
-                conn.sendall()
+          data = conn.recv(2048)
+          print 'Data received: ' + data.strip()
+          if self.killCmdRegEx.match(data):
+              print 'Shutting down...'
+              conn.sendall("Shutting down the Directory Server...")
+              sys.exit()
+          elif self.pathReqRegEx.match(data):
+              print 'file server data requested for a path'
+              pathKeyWord = "-path"
+              pathKeyWordStartIndex = data.find(pathKeyWord)
+              path = data[(pathKeyWordStartIndex + len(pathKeyWord)):]
+              path = path.strip()
+              fileServerAddress = ''
+              fileServerFileId = ''
+              fileServerAddress, fileServerFileId = self.getFileServerData(path)
+              if fileServerAddress is not None and fileServerFileId is not None:
+                  response = self.generateResponse(fileServerFileId + ";" + fileServerAddress)
+              else:
+                  response = self.generateResponse("101-ERROR: unnable to get file server data. Possibly directory server is unaware of any running file servers. ")
+              print 'sending response'
+              conn.sendall(response)
+          elif self.newFileServerBeacon.match(data):
+              address = data.split('=',1)[1]
+              address.strip()
+              print 'received new file server beacon from ' + address
+              self.fileServersList.append(address)
+              response = self.generateResponse("OK")
+              print 'sending an ack'
+              conn.sendall(response)
+          else:
+              print 'unsupported command received'
+              response = self.generateResponse("100-ERROR: unsupported command received")
+              conn.sendall(response)
     
     def pathExists(self, path):
-        exists = false
+        exists = False
         
         if path in self.directoriesDict:
-            exists = true
+            exists = True
             
         return exists
         
@@ -138,23 +127,23 @@ class DirectoryServer:
         address = ''
         fileId = ''
         
-        if pathExists(path)
+        if self.pathExists(path):
             print 'path already exists in the dictionary'
             serverData = str(directoriesDict[path])
             serverDataSplit = serverData.split(';',1)
             fileId = serverDataSplit[0]
             address = serverDataSplit[1]
-        elif
+        else:
             print 'new path to be added to the dictionary'
-            address = pickFileServer()
+            address = self.pickFileServer()
             if address is None:
                 print "Unable to pick file server"
                 return (None, None)
             else:
-                fileId = generateFileId()
+                fileId = self.generateFileId(path)
         return (address, fileId)
     
-    def generateFileId(self):
+    def generateFileId(self, path):
         fileId = hashlib.sha224(path).hexdigest()
         print 'generated new file server fileID: ' + fileId
         return fileId
@@ -163,17 +152,20 @@ class DirectoryServer:
         'picking file server randomly'
         fileServerListLength = len(self.fileServersList)
         if fileServerListLength > 0:
-            fileServerIndex = random.randint(0, fileServerListLength)
-        else
+            fileServerIndex = random.randint(0, fileServerListLength-1)
+        else:
             return None
         return self.fileServersList[fileServerIndex]
             
     def generateResponse(self, message):
-        return self.SERVER_NAME + "=" + message
+        return self.SERVER_NAME + "=" + message + "\n\n"
 
 def main():
     if len(sys.argv)!=3:
-        print 'Invalid number of arguments: 2 arguments required, port number to which bind the server and the size of the thread pool to be created.\nActual number of arguments: ' + str(len(sys.argv)-1)
+        print 'Invalid number of arguments: 2 arguments required.' +\
+           '\n-port number to which bind the server and the size of the thread pool to be created.' +\
+           '\n-size of the thread pool of the server' +\
+           '\nActual number of arguments: ' + str(len(sys.argv)-1)
         sys.exit()
     port = int(sys.argv[1])
     numThread = int(sys.argv[2])
